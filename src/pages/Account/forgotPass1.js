97f8e30ab68859a376c767dd2de8ec35347ef59d
JakobPalomo <jakobmichael.palomo.cics@ust.edu.ph>
2024-08-25 23:38:17 +0800

    forgot and change

--- src/pages/Account/forgotPass1.js
+++ src/pages/Account/forgotPass1.js
@@ -1,14 +1,14 @@
-import RegisterModal from "../../components/registerModal.js";
+import ForgotPass1 from "../../components/forgotPass1";
 import "../../css/registerModal.css";
 
-export default function Register() {
+export default function ForgotPass() {
   return (
     <div className="bg">
       <div className="headtext">
         <h1>Register Now!</h1>
       </div>
       <div className="modal-bg">
-        <RegisterModal />
+        <ForgotPass1 />
       </div>
     </div>
   );

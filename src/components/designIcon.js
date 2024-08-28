c2a411aaa18e01b2eeb3fe4d74ed256fd19aa9af
JakobPalomo <jakobmichael.palomo.cics@ust.edu.ph>
2024-08-28 17:25:07 +0800

    options button

--- src/components/designIcon.js
+++ src/components/designIcon.js
@@ -3,6 +3,11 @@ import "../css/homepage.css";
 function DesignIcon() {
   return (
     <div className="iconFrame">
+      <div className="options">
+        <h3 className="selectOption">
+          <center>&#8942;</center>
+        </h3>
+      </div>
       <img src="/img/logoWhitebg.png" className="pic" alt="No projects yet" />
 
       <h3 className="titleDesign">Project Name</h3>

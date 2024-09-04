ea07ca2d1b7952dd6088a8b656528c971fec65d8
JakobPalomo <jakobmichael.palomo.cics@ust.edu.ph>
2024-09-04 17:10:20 +0800

    protected route and login fixed

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// src/App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
@@ -10,25 +11,44 @@ import ForgotPass from "./pages/Account/forgotPass1.js";
 import Homepage from "./pages/Homepage/homepage.js";
 import Settings from "./pages/Settings/settings.js";
 import Design from "./pages/DesignSpace/Design.js";
+import { AuthProvider } from "./AuthContext"; // Adjust the path as necessary
+import ProtectedRoute from "./ProtectedRoute"; // Adjust the path as necessary
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route>
+    <AuthProvider>
+      <Router>
+        <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/" element={<Login />} />
           <Route path="/forgot" element={<ForgotPass />} />
           <Route path="/change" element={<ChangePassw />} />
-          <Route path="/users" element={<Users />} />
           <Route path="/otp" element={<OneTP />} />
+          {/* <Route
+            path="/homepage"
+            element={<ProtectedRoute element={<Homepage />} />}
+          />
+          <Route
+            path="/settings"
+            element={<ProtectedRoute element={<Settings />} />}
+          />
+          <Route
+            path="/design"
+            element={<ProtectedRoute element={<Design />} />}
+          />
+          <Route
+            path="/users"
+            element={<ProtectedRoute element={<Users />} />}
+          /> */}
           <Route path="/homepage" element={<Homepage />} />
           <Route path="/settings" element={<Settings />} />
-          <Route path="/design" element={<Design />} />
-        </Route>
-      </Routes>
-    </div>
+          <Route path="/design" element={<Design />} />{" "}
+          <Route path="/users" element={<Users />} />
+          {/* Add other routes as needed */}
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 

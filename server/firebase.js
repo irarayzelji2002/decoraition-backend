ba2048fe2080c8a1b9fc2bfa5371adb32d755903
JakobPalomo <jakobmichael.palomo.cics@ust.edu.ph>
2024-09-05 12:43:51 +0800

    made copies but couldnt move backend code yet

--- server/firebase.js
+++ server/firebase.js
@@ -1,35 +1,39 @@
-// Firebase ES module
-const { initializeApp } = require("firebase/app");
-const { getAnalytics } = require("firebase/analytics");
-const { getAuth } = require("firebase/auth");
-const { getFirestore } = require("firebase/firestore");
+import { initializeApp } from "firebase/app";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-const dotenv = require("dotenv").config({ path: "./.env" });
-const {
-  FRB_API_KEY,
-  FRB_AUTH_DOMAIN,
-  FRB_PROJECT_ID,
-  FRB_STORAGE_BUCKET,
-  FRB_MESSAGING_SENDER_ID,
-  FRB_APP_ID,
-  FRB_MEASUREMENT_ID,
-} = process.env;
-
-// Firebase configuration
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: FRB_API_KEY,
-  authDomain: FRB_AUTH_DOMAIN,
-  projectId: FRB_PROJECT_ID,
-  storageBucket: FRB_STORAGE_BUCKET,
-  messagingSenderId: FRB_MESSAGING_SENDER_ID,
-  appId: FRB_APP_ID,
-  measurementId: FRB_MEASUREMENT_ID,
+  apiKey: "AIzaSyDX78AGpu3POftTNfK0QSI4Og0nFlTq_6E",
+  authDomain: "decoraition.firebaseapp.com",
+  projectId: "decoraition",
+  storageBucket: "decoraition.appspot.com",
+  messagingSenderId: "637004924746",
+  appId: "1:637004924746:web:c7b833cf3577456472a3b3",
+  measurementId: "G-DQL41XE7BK",
 };
 
 // Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-const analytics = getAnalytics(firebaseApp);
-const auth = getAuth(firebaseApp);
-const db = getFirestore(firebaseApp);
+const app = initializeApp(firebaseConfig);
 
-module.exports = { firebaseApp, analytics, auth, db };
+export const handleForgotPassword = async (email) => {
+  const auth = getAuth();
+  try {
+    await sendPasswordResetEmail(auth, email);
+    alert("Password reset email sent! Check your inbox.");
+  } catch (error) {
+    console.error("Error sending password reset email:", error);
+    if (error.code === "auth/user-not-found") {
+      alert("No user found with this email.");
+    } else if (error.code === "auth/invalid-email") {
+      alert("Invalid email address.");
+    } else {
+      alert("Error sending password reset email.");
+    }
+  }
+};
+
+// Initialize Firebase Authentication and Firestore
+export const auth = getAuth(app);
+export const db = getFirestore(app); // Firestore initialization
+export default app;

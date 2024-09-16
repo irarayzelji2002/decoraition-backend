69990a2f166b3087e018664a27d2b04d8c7639a5
JakobPalomo <jakobmichael.palomo.cics@ust.edu.ph>
2024-09-16 21:46:40 +0800

    unused variables removed

--- src/components/ChangePass.js
+++ src/components/ChangePass.js
@@ -1,11 +1,11 @@
 import * as React from "react";
 import { useSearchParams } from "react-router-dom";
-import Avatar from "@mui/material/Avatar";
+
 import { useNavigate } from "react-router-dom";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";

aa9a593a00ac6de27a9b551877b840a0ae5d39a5
YnaSophiaDR <ynasophia.delrosario.cics@ust.edu.ph>
2024-10-15 18:39:52 +0800

    landing page update almost done

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,6 +1,39 @@
 import React from "react";
 import "../../css/landing.css";
 
+const features = [
+  {
+    img: "/img/ai-icon.png",
+    title: "AI helps you come up with design ideas",
+    description:
+      "Great for people who plan events, decorate homes, own businesses, and live in houses. DecorAItion Planner helps with all your decorating tasks. You can plan your designs if you're a pro or just like to do it yourself.",
+  },
+  {
+    img: "/img/easy-icon.png",
+    title: "Made for anyone",
+    description:
+      "Great for people who plan events, decorate homes, own businesses, and live in houses. DecorAItion Planner helps with all your decorating tasks. You can plan your designs if you're a pro or just like to do it yourself.",
+  },
+  {
+    img: "/img/team-icon.png",
+    title: "Team up and give feedback without stress",
+    description:
+      "Work with your team by sharing and handling designs in the app. Write comments and get feedback from peers. Join forces to make your ideas real.",
+  },
+  {
+    img: "/img/track-icon.png",
+    title: "Keep all your project plans in one place",
+    description:
+      "Take care of designs for venues or floors with tools that map spaces, show timelines, and help you manage your budget. Keep all your design plans neat and easy to find.",
+  },
+  {
+    img: "/img/track-icon.png",
+    title: "Track your design changes",
+    description:
+      "See all the changes made to your designs as time goes by. Bring back old versions without any trouble. Get your projects on your device with no fuss.",
+  },
+];
+
 const Landing = () => {
   return (
     <div className="landing-container">
@@ -16,9 +49,36 @@ const Landing = () => {
         <button className="download-btn">Login Now!</button>
       </div>
 
-      {/* <div className="image-grid">
-        <img src="/img/landing-image.png" alt="Design 1" className="grid-image"/>
-      </div> */}
+      <div className="features-section">
+        <h2>Why pick our app?</h2>
+        <div className="features-grid">
+          {features.map((feature, index) => (
+            <div
+              className={`feature feature-${index % 2 === 0 ? "odd" : "even"}`}
+              key={index}
+            >
+              <div className="feature-icon">
+                <img src={feature.img} alt={"icon"} />
+              </div>
+              <div>
+                <h3>{feature.title}</h3>
+                <p>{feature.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="makeover-section">
+        <h3>
+          Want to give your spaces a <span className="highlight">Makeover</span>
+          ?
+        </h3>
+        <p>
+          Get DecorAltion Planner today. Start your path to wonderfully designed
+          spaces now!
+        </p>
+        <button className="download-btn">Login Now</button>
+      </div>
     </div>
   );
 };
